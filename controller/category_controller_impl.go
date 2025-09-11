3d38024cd68f8888181df2681649681ad313e79f
Aaron Fabian <aaronfabian78@gmail.com>
2025-09-11 15:59:57 +0900

    Create new method for CategoryController Create and test case

--- controller/category_controller_impl.go
+++ controller/category_controller_impl.go
@@ -21,11 +21,8 @@ func (controller *CategoryControllerImpl) Create(ctx *fiber.Ctx) error {
 	// It's guaranteed to be not "", because restrict by tenant already did check first
 	tenantId, _ := strconv.Atoi(ctx.Params("tenantId"))
 
-	type BodyCategory struct {
-		CategoryName string `json:"category_name"`
-	}
 	type RequestBody struct {
-		Categories []*BodyCategory `json:"categories"`
+		Categories []string `json:"categories"`
 	}
 	var body RequestBody
 	err := ctx.BodyParser(&body)
@@ -34,11 +31,7 @@ func (controller *CategoryControllerImpl) Create(ctx *fiber.Ctx) error {
 			JSON(common.NewWebResponseError(400, common.StatusError, "Something gone wrong ! The request body is malformed"))
 	}
 
-	tobeCreatedCategories := make([]string, 0, len(body.Categories))
-	for _, category := range body.Categories {
-		tobeCreatedCategories = append(tobeCreatedCategories, category.CategoryName)
-	}
-	categories, err := controller.Service.Create(tenantId, tobeCreatedCategories)
+	categories, err := controller.Service.Create(tenantId, body.Categories)
 	if err != nil {
 		return ctx.Status(fiber.StatusBadRequest).
 			JSON(common.NewWebResponseError(400, common.StatusError, err.Error()))
--- controller/category_controller_impl_test.go
+++ controller/category_controller_impl_test.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	common "cashier-api/helper"
 	"cashier-api/helper/client"
 	constant "cashier-api/helper/constant/cookie"
 	"cashier-api/middleware"
@@ -9,6 +10,7 @@ import (
 	"cashier-api/service"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -18,7 +20,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/supabase-community/supabase-go"
 )
 
 func TestCategoryControllerImpl(t *testing.T) {
@@ -28,6 +32,13 @@ func TestCategoryControllerImpl(t *testing.T) {
 	testTimeout := int((time.Second * 5).Milliseconds())
 	app := fiber.New()
 	supabase := client.CreateSupabaseClient()
+	userRepository := repository.NewUserRepositoryImpl(supabase)
+	userService := service.NewUserServiceImpl(userRepository)
+	userController := NewUserControllerImpl(userService)
+	app.Post("/users/sign_in", userController.SignInWithEmailAndPassword)
+
+	// protected only login user
+	app.Use(middleware.ProtectedRoute)
 	categoryRepository := repository.NewCategoryRepositoryImpl(supabase)
 	categoryService := service.NewCategoryServiceImpl(categoryRepository)
 	categoryController := NewCategoryControllerImpl(categoryService)
@@ -72,10 +83,20 @@ func TestCategoryControllerImpl(t *testing.T) {
 	}
 	require.NotNil(t, enterprisePOSCookie)
 
+	// create items
+	// createdItems, err := createItems(supabase, []*model.Item{
+	// 	{
+	// 		ItemName: "Apple",
+	// 		Stocks:   10,
+	// 		TenantId: createdTenant.Id,
+	// 	},
+	// })
+	// require.NoError(t, err)
+
 	t.Run("Create", func(t *testing.T) {
 		t.Run("NormalCreate", func(t *testing.T) {
 			requestBody := fiber.Map{
-				"categories": []string{"Fast Food"},
+				"categories": []string{"Fast Food", "Japanese Food"},
 			}
 			byteBody, err = json.Marshal(&requestBody)
 			body = strings.NewReader(string(byteBody))
@@ -83,9 +104,102 @@ func TestCategoryControllerImpl(t *testing.T) {
 			request.Header.Set("Content-Type", "application/json")
 			request.AddCookie(enterprisePOSCookie)
 			response, err = app.Test(request, testTimeout)
-			require.Nil(t, err)
-			require.NotNil(t, response)
-			require.Equal(t, http.StatusOK, response.StatusCode)
+			assert.Nil(t, err)
+			assert.NotNil(t, response)
+			assert.Equal(t, http.StatusOK, response.StatusCode)
+
+			t.Cleanup(func() {
+				var getItemBody common.WebResponse
+				responseBody, err := common.ReadBody(response.Body)
+				require.NoError(t, err)
+				err = json.Unmarshal([]byte(responseBody), &getItemBody)
+				require.NoError(t, err)
+
+				dataMap, ok := getItemBody.Data.(map[string]interface{})
+				require.True(t, ok)
+
+				rawCategories, ok := dataMap["categories"].([]interface{})
+				require.True(t, ok)
+				require.NotEmpty(t, rawCategories)
+
+				// Marshal/unmarshal to proper type
+				var categories []*model.Category
+				rawBytes, err := json.Marshal(rawCategories)
+				require.NoError(t, err)
+
+				err = json.Unmarshal(rawBytes, &categories)
+				require.NoError(t, err)
+
+				_, count, err := supabase.From(repository.CategoryTable).
+					Delete("", "exact").
+					In("id", []string{fmt.Sprint(categories[0].Id), fmt.Sprint(categories[1].Id)}).
+					Execute()
+				require.NoError(t, err)
+				require.Equal(t, 2, int(count))
+			})
+		})
+
+		t.Run("EmptyRequest", func(t *testing.T) {
+			requestBody := fiber.Map{
+				"categories": []string{},
+			}
+			byteBody, err = json.Marshal(&requestBody)
+			body = strings.NewReader(string(byteBody))
+			request = httptest.NewRequest("POST", fmt.Sprintf("/categories/create/%d", createdTenant.Id), body)
+			request.Header.Set("Content-Type", "application/json")
+			request.AddCookie(enterprisePOSCookie)
+			response, err = app.Test(request, testTimeout)
+			assert.Nil(t, err)
+			assert.NotNil(t, response)
+			assert.Equal(t, http.StatusBadRequest, response.StatusCode) // 400
+		})
+
+		t.Run("SameCategoryName", func(t *testing.T) {
+			requestBody := fiber.Map{
+				"categories": []string{"Same Category", "Same Category"},
+			}
+			byteBody, err = json.Marshal(&requestBody)
+			body = strings.NewReader(string(byteBody))
+			request = httptest.NewRequest("POST", fmt.Sprintf("/categories/create/%d", createdTenant.Id), body)
+			request.Header.Set("Content-Type", "application/json")
+			request.AddCookie(enterprisePOSCookie)
+			response, err = app.Test(request, testTimeout)
+			assert.Nil(t, err)
+			assert.NotNil(t, response)
+			assert.Equal(t, http.StatusBadRequest, response.StatusCode) // 400
+
+			body, err := io.ReadAll(response.Body)
+			assert.NoError(t, err)
+			assert.Contains(t, string(body), "Something gone wrong. Duplicate category detected")
 		})
 	})
+
+	t.Cleanup(func() {
+		_, _, err = supabase.From(repository.UserMtmTenantTable).
+			Delete("", "").
+			Eq("user_id", fmt.Sprint(createdTestUser.Id)).
+			Eq("tenant_id", fmt.Sprint(createdTenant.Id)).
+			Execute()
+		require.NoError(t, err, "If this fail, then immediately delete the data from TestWarehouseControllerImpl (1)")
+		_, _, err = supabase.From(repository.TenantTable).
+			Delete("", "").
+			Eq("id", fmt.Sprint(createdTenant.Id)).
+			Execute()
+		require.NoError(t, err, "If this fail, then immediately delete the data from TestWarehouseControllerImpl (2)")
+		_, _, err = supabase.From(repository.UserTable).
+			Delete("", "").
+			Eq("id", fmt.Sprint(createdTestUser.Id)).
+			Execute()
+		require.NoError(t, err, "If this fail, then immediately delete the data from TestWarehouseControllerImpl (3)")
+	})
+}
+
+func createItems(supabase *supabase.Client, items []*model.Item) ([]*model.Item, error) {
+	var results []*model.Item
+	_, err := supabase.From(repository.WarehouseTable).Insert(items, false, "", "", "").ExecuteTo(&results)
+	if err != nil {
+		return nil, err
+	} else {
+		return results, nil
+	}
 }
