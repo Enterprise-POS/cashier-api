5cded18d8e9e96f0016fa45ac2966a8ebeb97308
Aaron Fabian <aaronfabian78@gmail.com>
2026-09-05 18:36:39 +0900

    Update documentation for PurchasedItem model

--- model/purchased_item_list.go
+++ model/purchased_item_list.go
@@ -7,16 +7,22 @@ You can't add row to purchased_item_list
 unless you create order_item first
 */
 type PurchasedItem struct {
-	Id                 int        `json:"id,omitempty" gorm:"primaryKey;autoIncrement;column:id"`
-	Quantity           int        `json:"quantity" gorm:"column:quantity"`
-	StorePriceSnapshot int        `json:"store_price_snapshot" gorm:"column:store_price_snapshot"`
-	BasePriceSnapshot  int        `json:"base_price_snapshot" gorm:"column:base_price_snapshot"`
-	DiscountAmount     int        `json:"discount_amount" gorm:"column:discount_amount"`
-	TotalAmount        int        `json:"total_amount" gorm:"column:total_amount"`
-	CreatedAt          *time.Time `json:"created_at,omitempty" gorm:"column:created_at;<-:create"`
-	ItemId             int        `json:"item_id" gorm:"column:item_id"`
-	ItemNameSnapshot   string     `json:"item_name_snapshot" gorm:"column:item_name_snapshot"`
-	OrderItemId        int        `json:"order_item_id" gorm:"column:order_item_id"`
+	Id                 int    `json:"id,omitempty" gorm:"primaryKey;autoIncrement;column:id"`
+	Quantity           int    `json:"quantity" gorm:"column:quantity"`
+	StorePriceSnapshot int    `json:"store_price_snapshot" gorm:"column:store_price_snapshot"`
+	BasePriceSnapshot  int    `json:"base_price_snapshot" gorm:"column:base_price_snapshot"`
+	DiscountAmount     int    `json:"discount_amount" gorm:"column:discount_amount"`
+	TotalAmount        int    `json:"total_amount" gorm:"column:total_amount"`
+	ItemId             int    `json:"item_id" gorm:"column:item_id"`
+	ItemNameSnapshot   string `json:"item_name_snapshot" gorm:"column:item_name_snapshot"`
+	OrderItemId        int    `json:"order_item_id" gorm:"column:order_item_id"`
+
+	// Sometimes when we get the data about order_item and the detail purchased_item_list, we don't want
+	// the time for PurchasedItem because it may be redundant. In lang such as Kotlin or TypeScript we may want
+	// mark this property for example -> created_at: Date?
+	// however want save the data this property is a must, but gorm already create automatically for us
+	// see order_item_repository_impl too see that purchased_item_list created_at is not specified
+	CreatedAt *time.Time `json:"created_at,omitempty" gorm:"column:created_at;<-:create"`
 
 	// This query below is only for JOIN to warehouse and order_item
 	ItemName           string     `json:"item_name,omitempty" gorm:"column:item_name;->"`
