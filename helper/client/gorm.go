f09ad61bad569a865c70388098b14e6af59e5376
Aaron Fabian <aaronfabian78@gmail.com>
2026-09-21 18:52:17 +0900

    Edit configuration for gorm

--- helper/client/gorm.go
+++ helper/client/gorm.go
@@ -16,10 +16,21 @@ var (
 	DB_PASSWORD string = os.Getenv("DB_PASSWORD")
 	DB_PORT     string = os.Getenv("DB_PORT")
 	DB_TIMEZONE string = os.Getenv("DB_TIMEZONE")
+
+	DEV_DB_HOST     string = os.Getenv("DEV_DB_HOST")
+	DEV_DB_USER     string = os.Getenv("DEV_DB_USER")
+	DEV_DB_PASSWORD string = os.Getenv("DEV_DB_PASSWORD")
+	DEV_DB_PORT     string = os.Getenv("DEV_DB_PORT")
+	DEV_DB_TIMEZONE string = os.Getenv("DEV_DB_TIMEZONE")
 )
 
 func CreateGormClient() *gorm.DB {
-	dialect := fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=require TimeZone=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_TIMEZONE)
+	var dialect string
+	if MODE == "prod" {
+		dialect = fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=require TimeZone=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_TIMEZONE)
+	} else {
+		dialect = fmt.Sprintf("host=%s user=%s password=%s dbname=postgres port=%s sslmode=require TimeZone=%s", DEV_DB_HOST, DEV_DB_USER, DEV_DB_PASSWORD, DEV_DB_PORT, DEV_DB_TIMEZONE)
+	}
 
 	logMode := logger.Warn
 	if MODE == "prod" {
